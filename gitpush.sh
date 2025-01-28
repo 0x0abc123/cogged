6146833a2b3b3f95c9d1a5ff25f1fc65abb0df35
0x0abc123 <71738504+0x0abc123@users.noreply.github.com>
2025-01-28 21:11:30 +1100

    Commit 1738059089

--- gitpush.sh
+++ gitpush.sh
@@ -3,6 +3,6 @@
 MSG=""
 if [[ "$1" ]] ; then MSG="$1" ; else MSG="Commit $(date +%s)" ; fi
 git add --all
-git commit -am "Commit $MSG"
+git commit -am "$MSG"
 git push git@github.com:0x0abc123/cogged.git main
 
